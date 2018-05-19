8c0d820007c418d6a0f5bbf014a39aa8cb74d8c4
James Hosken <jameswfhosken@gmail.com>
2018-05-19 22:38:24 +0100

    Fix bug in api keys path
    I forgot a slash

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
-var keys = require(".keys/keys.js");
+var keys = require("./keys/keys.js");
 
 console.log(keys.getAPIKey())
 console.log(keys.getAPISecret())
 console.log(keys.getAccessToken())
 console.log(keys.getAccessTokenSecret())
+
+
