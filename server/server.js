dc2ac05b2fc847f94b08fcb4a1839bd68e1b42ba
James Hosken <jameswfhosken@gmail.com>
2018-05-19 22:44:57 +0100

    Add basic twitter stream listener
    Invaluably supplied by https://github.com/ttezel/twit

--- server/server.js
+++ server/server.js
@@ -6,3 +6,20 @@ console.log(keys.getAccessToken())
 console.log(keys.getAccessTokenSecret())
 
 
+var Twit = require('twit')
+
+var T = new Twit({
+  consumer_key:         keys.getAPIKey(),
+  consumer_secret:      keys.getAPISecret(),
+  access_token:         keys.getAccessToken(),
+  access_token_secret:  keys.getAccessTokenSecret(),
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+})
+
+var stream = T.stream('statuses/filter', { track: 'elephant' })
+
+stream.on('tweet', function (tweet) {
+  console.log("Text: " + tweet.text)
+  console.log("By: " + tweet.user.name)
+  console.log("--- END ---");
+})
