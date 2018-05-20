9f1d2ba1166d233b1764b6c4a66734962b5378ca
James Hosken <jameswfhosken@gmail.com>
2018-05-20 03:08:00 +0100

    Add static map to server and send to Unity to parse

---
+++ Twitter Controlled Game/Assets/MapData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapData : MonoBehaviour {
+
+    public int mapData;
+
+
+    public static MapData CreateFromJSON(string json)
+    {
+        return JsonUtility.FromJson<MapData>(json);
+    }
+}
